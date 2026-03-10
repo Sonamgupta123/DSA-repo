54686c10dbfc83ee2cef676532aa2fef6488c28f
Sonam Gupta <sonamflower34@gmail.com>
2026-03-10 17:41:55 +0000

    updating pattern.cpp

--- pattern.cpp
+++ pattern.cpp
@@ -1,13 +1,15 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n;
+    int n ;
     cout<<"enter the number of rows"<<endl;
     cin>>n;
-    for(int i=1;i<=n;i++){
-      for(int j=1 ; j<=n ;j++){
+    
+    for(int i=1;i<n;i++){
+      for(int j=1 ; j<n ;j++){
         cout<<"*";
       }
       cout<<endl;
     }
+    return 0;
 }
