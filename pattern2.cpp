6e618e6c454445d5c021e32bcabd9dd2230a237c
Sonam Gupta <sonamflower34@gmail.com>
2026-03-11 18:07:16 +0000

    more concept in pattern2.cpp

--- pattern2.cpp
+++ pattern2.cpp
@@ -1,11 +1,13 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n = 4;
-
+    int n = 3;
+    
+   
     for(int i=1 ; i<=n; i++){
-        for(int j=1 ;j<=n ;j++){
-            cout<<j;
+        for(int j=1 ;j<=i ;j++){
+            cout<<"*";
+          
         }
         cout<<endl;
     }
