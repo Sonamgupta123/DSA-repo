8d0c871526a8a15da59b5927aabd7b1d10ddf33a
Sonam Gupta <sonamflower34@gmail.com>
2026-03-12 18:29:36 +0000

    updating Factorial_Fun .cpp

--- FactorialFun.cpp
+++ FactorialFun.cpp
@@ -17,11 +17,18 @@ int sum(int n){
 int sum1(int a, int b){
     return a+b;
 }
+int chnageX(int x){
+    x = 2*x;
+    cout<<"x = "<<x<<endl;
+}
 int main(){
-    int x = 5 , y = 4;
-    sum1(x,y);
+    int a = 5 , b = 4 x = 5;
+    
+    sum1(a,b);
     cout<<"factorial is "<<fact(4)<<endl;
     cout<<"sum is "<<sum(10)<<endl;
-    cout<<"sum1 is "<<sum1(x,y)<<endl;
+    cout<<"sum1 is "<<sum1(a,b)<<endl;
+    chnageX(x);
+    cout<<"x = "<<x<<endl;
     return 0;
 }
