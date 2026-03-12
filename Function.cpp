a62a01ac12fc8e8aa34d727369451b31007fb6ee
Sonam Gupta <sonamflower34@gmail.com>
2026-03-12 18:13:37 +0000

    updating Function.cpp

--- Function.cpp
+++ Function.cpp
@@ -4,7 +4,24 @@ int printHello(){
     cout<<"hello\n";
     return 3;
 }
+int sum(int a, int b){
+  int s = a+b;
+  return s;
+}
+
+int minNum(int a , int b){
+    if(a<b){
+        
+        return a;
+    }else{
+        
+        return b;
+    }
+    
+}
 int main(){
+    cout<<sum(10,5)<<endl;
     cout<< printHello() <<endl;
+    cout<<"min is"<< minNum(10,80)<<endl;
     return 0;
 }
