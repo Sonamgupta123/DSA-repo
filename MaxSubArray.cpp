8c6d28055200ce8bf4fde1b99eb088a5759301d2
Sonam Gupta <sonamflower34@gmail.com>
2026-03-17 14:49:12 +0000

    updating MaxSumArray.cpp

--- MaxSubArray.cpp
+++ MaxSubArray.cpp
@@ -1,16 +1,37 @@
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// int main(){
+//     int n = 5;
+//     int arr[5] = {1,2,3,4,5};
+   
+//     for(int st=0 ; st<n ;st++){
+//         for(int end = st ; end<n ;end++){
+//             for(int i = st; i<=end;i++){
+//                 cout<<arr[i];
+//             }
+//             cout<<" ";
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
 #include<iostream>
+#include<vector>
+#include<climits>
 using namespace std;
 int main(){
     int n = 5;
     int arr[5] = {1,2,3,4,5};
+   int maxSum = INT_MIN;
     for(int st=0 ; st<n ;st++){
+        int currSum = 0;
         for(int end = st ; end<n ;end++){
-            for(int i = st; i<=end;i++){
-                cout<<arr[i];
-            }
-            cout<<" ";
-        }
-        cout<<endl;
+           currSum += arr[end];
+           maxSum = max(currSum,maxSum);
+            
+        }     
     }
+    cout<<"max subarray sum = "<<maxSum<<endl;
     return 0;
 }
