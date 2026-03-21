4caaffa3cda99c1caee7bde39bdb5e78da583024
Sonam Gupta <sonamflower34@gmail.com>
2026-03-21 16:26:43 +0000

    updating maximum amount of water in container question

--- AmountWater.cpp
+++ AmountWater.cpp
@@ -1,22 +1,33 @@
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// int maxArea(vector<int>& height) {
+//         int maxWater = 0;
+//         for(int i=0 ;i<height.size();i++){
+//             for(int j =i+1 ; j<height.size();j++){
+//                 int w = j-i;
+//                 int ht = min(height[i],height[j]);
+//                 int currWater = w*ht;
+
+//                 maxWater = max(maxWater,currWater);
+//             }
+//         }
+//         return maxWater;
+//     }
+// int main(){
+//     vector<int> height ={1,8,6,2,5,4,8,3,7};
+
+//     cout<<maxArea(height)<<endl;
+//     return 0;
+// }
+
+/// Two pointer approach in this
 #include<iostream>
-#include<vector>
 using namespace std;
 int maxArea(vector<int>& height) {
-        int maxWater = 0;
-        for(int i=0 ;i<height.size();i++){
-            for(int j =i+1 ; j<height.size();j++){
-                int w = j-i;
-                int ht = min(height[i],height[j]);
-                int currWater = w*ht;
-
-                maxWater = max(maxWater,currWater);
-            }
-        }
-        return maxWater;
-    }
+    int lp = 0 , rp = n-1 , ans = 0;
+    
+}
 int main(){
-    vector<int> height ={1,8,6,2,5,4,8,3,7};
-
-    cout<<maxArea(height)<<endl;
     return 0;
 }
