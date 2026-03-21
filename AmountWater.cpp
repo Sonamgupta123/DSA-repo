f5eae3b3ca711311196981eee4ce739d8f5b1bed
Sonam Gupta <sonamflower34@gmail.com>
2026-03-21 16:14:10 +0000

    initial_commit

--- AmountWater.cpp
+++ AmountWater.cpp
@@ -1,6 +1,18 @@
 #include<iostream>
 using namespace std;
+int maxArea(vector<int>& height) {
+        int maxWater = 0;
+        for(int i=0 ;i<height.size();i++){
+            for(int j =i+1 ; j<height.size();j++){
+                int w = j-i;
+                int ht = min(height[i],height[j]);
+                int currWater = w*ht;
 
+                maxWater = min(maxWater,currWater);
+            }
+        }
+        return maxWater;
+    }
 int main(){
     return 0;
 }
