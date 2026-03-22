17c333ebe60fe22f0887ae4f9f83b14b850c8b52
Sonam Gupta <sonamflower34@gmail.com>
2026-03-22 16:09:28 +0000

    updating_AmountWater.cpp

--- AmountWater.cpp
+++ AmountWater.cpp
@@ -23,11 +23,23 @@
 
 /// Two pointer approach in this
 #include<iostream>
+#include<vector>
 using namespace std;
 int maxArea(vector<int>& height) {
-    int lp = 0 , rp = n-1 , ans = 0;
-    
+    int lp = 0 , rp = height.size()-1 , maxWater = 0;
+ while(lp<rp){
+    int w = rp - lp;
+    int ht = min (height[lp] , height[rp]);
+    int currWater = w* ht;
+    maxWater = max(maxWater,currWater);
+    height[lp]< height[rp] ? lp++ : rp--;
+
+ }
+ return maxWater;
 }
 int main(){
+    vector<int> height ={1,8,6,2,5,4,8,3,7};
+
+    cout<<maxArea(height)<<endl;
     return 0;
 }
