b101068c5a7fd79c01e623781740d20d8509f7b5
Sonam Gupta <sonamflower34@gmail.com>
2026-03-22 17:42:12 +0000

    adding ProductArr.cpp file

--- ProductArr.cpp
+++ ProductArr.cpp
@@ -1,19 +1,40 @@
+//product of array uske kudh ke index wale number ko chd kar baaki ka product 
+
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// vector<int> productExceptSelf(vector<int>& nums) {
+//     int n = nums.size();
+//     vector<int> ans(n,1);
+//         for(int i =0 ; i<n;i++){
+//             int product = 1;
+//             for(int j=0; j<n;j++){
+//                 if(i!=j){
+//                product*= nums[j];
+//                 }
+//             }
+//             ans[i]= product;
+//         }
+//         return ans;
+//     }
+// int main(){
+//     vector<int> nums = {1,2,3,4};
+//       vector<int> res = productExceptSelf(nums);
+
+//     for(int x : res){
+//         cout << x << " ";
+//     }
+
+//     return 0;
+// }
+
+// now optimised solution
+
 #include<iostream>
 #include<vector>
 using namespace std;
-vector<int> productExceptSelf(vector<int>& nums) {
-    int n = nums.size();
-    vector<int> ans(n,1);
-        for(int i =0 ; i<n;i++){
-            int product = 1;
-            for(int j=0; j<n;j++){
-                if(i!=j){
-               product*= nums[j];
-                }
-            }
-            ans[i]= product;
-        }
-        return ans;
+ vector<int> productExceptSelf(vector<int>& nums) {
+        
     }
 int main(){
     vector<int> nums = {1,2,3,4};
