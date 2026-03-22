709a038aec68b6af1639ab1e0179adb2cd292f15
Sonam Gupta <sonamflower34@gmail.com>
2026-03-22 16:25:09 +0000

    initial2 commit

--- ProductArr.cpp
+++ ProductArr.cpp
@@ -1,6 +1,18 @@
 #include<iostream>
+#include<vector>
 using namespace std;
-
+vector<int> productExceptSelf(vector<int>& nums) {
+    vector<int> ans;
+        for(int i =0 ; i<nums.size();i++){
+            int product = 1;
+            for(j=0; j<nums.size();j++){
+                if(i!=j){
+               product *= nums[j];
+                }
+               ans[i] = product;
+            }
+        }
+    }
 int main(){
     return 0;
 }
