66ef9320c3011d374d8b66bfb9b5d7072a9b74d1
Sonam Gupta <sonamflower34@gmail.com>
2026-03-22 16:27:22 +0000

    initial3 commit

--- ProductArr.cpp
+++ ProductArr.cpp
@@ -2,17 +2,21 @@
 #include<vector>
 using namespace std;
 vector<int> productExceptSelf(vector<int>& nums) {
-    vector<int> ans;
-        for(int i =0 ; i<nums.size();i++){
+    int n = nums.size();
+    vector<int> ans(n,1);
+        for(int i =0 ; i<n;i++){
             int product = 1;
-            for(j=0; j<nums.size();j++){
+            for(j=0; j<n;j++){
                 if(i!=j){
                product *= nums[j];
                 }
-               ans[i] = product;
             }
+               ans[i] = product;
         }
+        return ans;
     }
 int main(){
+    vector<int> nums = {1,2,3,4}
+    cout<<productExceptSelf(nums)<<endl;
     return 0;
 }
