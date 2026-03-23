17a2e44ff9c4cb6969c8848905afadfcb1f2cd7a
Sonam Gupta <sonamflower34@gmail.com>
2026-03-23 15:10:39 +0000

    adding commit

--- ArrayPointer.cpp
+++ ArrayPointer.cpp
@@ -3,6 +3,13 @@
 using namespace std;
 int main(){
     int arr[] = {1,2,3,4,5};
-    cout<<*arr<<endl;//1
+    // cout<<*arr<<endl;//1
+    int a = 10;
+    int *ptr = &a;
+    cout<<ptr <<endl;
+    ptr--;
+    cout<<ptr<<endl;
+    cout<<*(arr+1)<<endl;//2
+     cout<<*(arr+2)<<endl;//3
     return 0;
 }
