f231c057ab48f10bdb0417ae12a0feea9effc4b3
Sonam Gupta <sonamflower34@gmail.com>
2026-03-23 14:40:41 +0000

    initial3 commit

--- Pointer1.cpp
+++ Pointer1.cpp
@@ -4,8 +4,11 @@ using namespace std;
 int main(){
     int a = 10;
     int *ptr = &a;
+
+    int ** parptr = &ptr;// pointer to pointer 
     cout<<ptr<<endl;
     cout<< &a <<endl;
     cout<< &ptr<<endl;
+    cout<< parptr<<endl;
     return 0;
 }
