a4099b284523aea42bc3c25b342b417401f8a11d
Sonam Gupta <sonamflower34@gmail.com>
2026-03-24 16:21:47 +0000

    adding recursion binary search commit

--- BinarySearchRecurr.cpp
+++ BinarySearchRecurr.cpp
@@ -1,7 +1,22 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-
+int recBinSearch(vector<int> arr , int tar , int st , int end){
+    if(st<=end){
+        int mid = st + (end-st)/2;
+        if(tar>arr[mid]){
+            return recBinSearch(arr, tar,mid-1,end);
+        }else if(tar<arr[end]){
+            return recBinSearch(arr,tar,st,mid-1);
+        }else {
+            return mid;
+        }
+    }
+    return -1;
+}
 int main(){
+    vector<int> arr1 = {-1,0,3,4,5,9,12};
+    int tar1 = 4;
+    cout<< recBinSearch(arr1,tar1)<<endl;
     return 0;
 }
