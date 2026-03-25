501824ca4f3951f098a99042669dd9aa8456341f
Sonam Gupta <sonamflower34@gmail.com>
2026-03-25 18:12:29 +0000

    adding file commit

--- rotatedArraySearch.cpp
+++ rotatedArraySearch.cpp
@@ -2,29 +2,37 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-int search(vector<int>& nums, int target) {
-        int st =  0 , end = nums.size()-1;
-        while(st<=end){
-            int mid = st + (end-st)/2;
+
+    int search(vector<int>& nums, int target) {
+        int st = 0, end = nums.size() - 1;
+
+        while(st <= end){
+            int mid = st + (end - st) / 2;
+
             if(nums[mid] == target){
                 return mid;
             }
-            if(nums[st]<= nums[mid]){
-                if(nums[st]<= target && target <= nums[mid]){
-                    end = mid-1;
-                }else{
-                    st = mid+1;
+
+            // left sorted
+            if(nums[st] <= nums[mid]){
+                if(nums[st] <= target && target < nums[mid]){
+                    end = mid - 1;
+                } else {
+                    st = mid + 1;
                 }
-            }else{
-                if(nums[mid]<=target && target <= nums[mid]){
-                    st = mid+1;
-                }else{
-                    end = mid-1;
+            }
+            // right sorted
+            else{
+                if(nums[mid] < target && target <= nums[end]){
+                    st = mid + 1;
+                } else {
+                    end = mid - 1;
                 }
             }
         }
         return -1;
     }
+
 int main(){
  vector<int> nums ={4,5,6,7,0,1,2};
  cout <<search(nums, 6)<<endl;
