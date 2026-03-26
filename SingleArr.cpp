86f4e21a58306218b6ca867ecb5bedab51e2353e
Sonam Gupta <sonamflower34@gmail.com>
2026-03-26 18:39:29 +0000

    final commit

--- SingleArr.cpp
+++ SingleArr.cpp
@@ -8,10 +8,29 @@ using namespace std;
         int st = 0 , end = n-1;
         while(st<=end){
             int mid = st + (end-st)/2;
-            
+            if(mid==0 && nums[0]!= nums[1]) return nums[mid];
+            if(mid == n-1 && nums[n-1] != nums[n-2]) return nums[mid];
+
+            if(nums[mid-1] != nums[mid] && nums[mid] != nums[mid+1]) return nums[mid];
+
+            if(mid%2 == 0){
+                if(nums[mid-1] == nums[mid]){
+                    end = mid - 1;
+                }else{
+                    st = mid+1;
+                }
+            }else{
+                if(nums[mid-1] == nums[mid]){
+                    st = mid+1;
+                }else{
+                    end = mid-1;
+                }
+            }
         }
+        return -1;
     }
 int main(){
-    
+    vector<int> nums = {1,1,2,2,3,4,4,5,5};
+    cout<<singleNonDuplicate(nums)<<endl;
     return 0;
 }
