ac1c3075e503e32d7ca2b56fd16312e8bc1b65a3
Sonam Gupta <sonamflower34@gmail.com>
2026-03-26 18:29:13 +0000

    initial3 commit

--- SingleArr.cpp
+++ SingleArr.cpp
@@ -3,9 +3,12 @@
 #include<vector>
 using namespace std;
  int singleNonDuplicate(vector<int>& nums) {
-        int st = 0 , end = nums.size()-1;
+    int n = nums.size();
+    if(n== 1) return nums[0];
+        int st = 0 , end = n-1;
         while(st<=end){
             int mid = st + (end-st)/2;
+            
         }
     }
 int main(){
