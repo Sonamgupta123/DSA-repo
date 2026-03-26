060ed40992df63652b2d37e9523f7d8e293ff8eb
Sonam Gupta <sonamflower34@gmail.com>
2026-03-26 18:10:05 +0000

    initial1 commit

--- SingleArr.cpp
+++ SingleArr.cpp
@@ -1,6 +1,10 @@
+// repeated value mai se sirf ek single value de from an array
 #include<iostream>
 #include<vector>
 using namespace std;
+ int singleNonDuplicate(vector<int>& nums) {
+        
+    }
 int main(){
     
     return 0;
