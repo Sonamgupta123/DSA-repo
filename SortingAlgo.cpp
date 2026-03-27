6894f0222d9f4df866f3d0cbdab63ce6bcc2b21d
Sonam Gupta <sonamflower34@gmail.com>
2026-03-27 17:36:58 +0000

    inital 5 commit

--- SortingAlgo.cpp
+++ SortingAlgo.cpp
@@ -10,8 +10,16 @@ for(int i = 0 ; i<n-1;i++){
     }
 }
 }
+void printArray(int arr[] , int n ){
+    for(int i = 0 ;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
 int main(){
     int n = 5;
     int arr[] = {4,1,5,2,3};
+    bubbleShort(arr,n);
+    printArray(arr,n);
     return 0;
 }
