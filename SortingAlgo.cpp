09a7392db9ff2f9ce8b7e1b9602d5af4cb75a76b
Sonam Gupta <sonamflower34@gmail.com>
2026-03-27 17:40:30 +0000

    inital 7 commit

--- SortingAlgo.cpp
+++ SortingAlgo.cpp
@@ -9,7 +9,11 @@ for(int i = 0 ; i<n-1;i++){
     for(int j = 0; j<n-i-1;j++){
         if(arr[j]>arr[j+1]){
             swap(arr[j],arr[j+1]);
+            isSwap = true;
         }
+    }
+    if(!isSwap){// return sorted element
+        return;
     }
 }
 }
