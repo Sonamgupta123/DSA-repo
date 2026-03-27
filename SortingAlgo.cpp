f67981eea6c004a696eecd2f2d1010edee06bb45
Sonam Gupta <sonamflower34@gmail.com>
2026-03-27 17:34:38 +0000

    inital 4 commit

--- SortingAlgo.cpp
+++ SortingAlgo.cpp
@@ -1,7 +1,17 @@
 //Bubble short swap karte hai values ko phale wale value se agar bada hai toh
 #include<iostream>
-#include<vector>
 using namespace std;
+void bubbleShort(int arr[], int n){// O(n^2)
+for(int i = 0 ; i<n-1;i++){
+    for(int j = 0; j<n-i-1;j++){
+        if(arr[j]>arr[j+1]){
+            swap(arr[j],arr[j+1]);
+        }
+    }
+}
+}
 int main(){
+    int n = 5;
+    int arr[] = {4,1,5,2,3};
     return 0;
 }
