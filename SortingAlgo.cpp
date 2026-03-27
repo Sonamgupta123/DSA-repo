691c36908e0b306f0243e9756a7bc99b416d5bfc
Sonam Gupta <sonamflower34@gmail.com>
2026-03-27 17:39:19 +0000

    inital 6 commit

--- SortingAlgo.cpp
+++ SortingAlgo.cpp
@@ -1,8 +1,11 @@
 //Bubble short swap karte hai values ko phale wale value se agar bada hai toh
+// buuble sort sorted element denge tbhi bhi n time loop run karega sort karne ke liye
 #include<iostream>
 using namespace std;
 void bubbleShort(int arr[], int n){// O(n^2)
 for(int i = 0 ; i<n-1;i++){
+    bool isSwap = false; // check karega ki element sort ho gye toh loop rok dega 
+
     for(int j = 0; j<n-i-1;j++){
         if(arr[j]>arr[j+1]){
             swap(arr[j],arr[j+1]);
