82c1790465fbe93e0420f04a74389591203b5f2e
Sonam Gupta <sonamflower34@gmail.com>
2026-03-27 17:55:52 +0000

    inital 10 commit

--- SortingAlgo.cpp
+++ SortingAlgo.cpp
@@ -30,7 +30,7 @@ void selectionSort(int arr[] , int n){
  for(int i=0 ; i<n-1 ;i++){
     int smallestIndx = i;
     for(int j = i+1 ; j<n ;j++){
-        if(arr[j]<arr(smallestIndx)){
+        if(arr[j]<arr[smallestIndx]){
             smallestIndx = j;
         }
     }
