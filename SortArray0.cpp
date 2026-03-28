165b5badb70b340b6ac56af255e07c9776c5433b
Sonam Gupta <sonamflower34@gmail.com>
2026-03-28 18:08:51 +0000

    initial 2 commit

--- SortArray0.cpp
+++ SortArray0.cpp
@@ -1,8 +1,13 @@
 #include<iostream>
+#include<vector>
+
 using namespace std;
 void sortColors(vector<int>& nums) {
-        
+        // sort using bruteforce apporoch
+        sort(nums.begin(),nums.end());
     }
 int main(){
+    vector<int> nums = {0,1,2,1,2,1,0,0,2};
+    cout<<sortColors(nums)<<endl;
     return 0;
 }
