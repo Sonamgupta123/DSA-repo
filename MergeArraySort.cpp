55061d6d79dd94a61ad06e8ba259b4b989dfeb8d
Sonam Gupta <sonamflower34@gmail.com>
2026-03-29 17:56:39 +0000

    adding2 5

--- MergeArraySort.cpp
+++ MergeArraySort.cpp
@@ -1,8 +1,18 @@
 #include<iostream>
 #include<vector>
 using namespace std;
- void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        
+ void merge(vector<int>& A, int m, vector<int>& B, int n) {
+        int idx = m+n-1 , i = m-1 , j = n-1;
+        while(i>=0 && j>=0){
+            if(A[i]>= B[j]){
+                A[idx--] = A[i--];
+            }else {
+                A[idx--] = B[j--];
+            }
+        }
+        while(j>=0){
+            A[idx--] = B[j--]; 
+        }
     }
 int main(){
 vector<int> nums1 = {1,2,3,0,0,0};
