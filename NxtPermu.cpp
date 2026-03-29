f1624d3f9f7c59030dc1fa43ebda2aa6e2f2b1ff
Sonam Gupta <sonamflower34@gmail.com>
2026-03-29 18:05:41 +0000

    initial commit

--- NxtPermu.cpp
+++ NxtPermu.cpp
@@ -1,3 +1,4 @@
+//ek hi array ke pattern ya alag alag pattern like 123 132 213 231 312 321 etc
 #include<iostream>
 #include<vector>
 using namespace std;
@@ -5,6 +6,10 @@ void nextPermutation(vector<int>& nums) {
         
     }
 int main(){
-
+ vector<int> nums = {1,2,3};
+ nextPermutation(nums);
+ for(int x : nums){
+     cout << x << " ";
+ }
     return 0;
 }
