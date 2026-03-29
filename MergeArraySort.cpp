8c458e64915e0480e7e34611f7509a648207845f
Sonam Gupta <sonamflower34@gmail.com>
2026-03-29 17:46:59 +0000

    adding2 4

--- MergeArraySort.cpp
+++ MergeArraySort.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
+#include<vector>
 using namespace std;
  void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         
     }
 int main(){
+vector<int> nums1 = {1,2,3,0,0,0};
+vector<int> num2 = {2,5,6};
 return 0;
 }
