3f5e34d4343dbe80ced9c2f026253916e8b596af
Sonam Gupta <sonamflower34@gmail.com>
2026-03-30 17:53:30 +0000

    inital commit 1

--- Stl.cpp
+++ Stl.cpp
@@ -3,16 +3,19 @@
 #include<vector>
 using namespace std;
 int main(){
-    vector<int> vec;
-    vec.push_back(1) ;
-    vec.push_back(2);
-    vec.push_back(3);
-    vec.push_back(4);
-    vec.pop_back(4);
+    vector<int> vec ={1,2,3,4};
+    // vec.push_back(1) ;
+    // vec.push_back(2);
+    // vec.push_back(3);
+    // vec.push_back(4);
+    // vec.emplace_back(5);
+    // vec.pop_back();
     cout<<" size "<<vec.size()<<endl;
     cout<<"capacity "<<vec.capacity()<<endl;
     for(int val : vec){
         cout<<val<<" ";
     }
+    //vector<int> vec(3,10);
+    //vector<int> vec2(vec1);
     return 0;
 }
