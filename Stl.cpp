017b09a7dbf45629e13359b41e0d3bec4bada648
Sonam Gupta <sonamflower34@gmail.com>
2026-03-30 16:57:09 +0000

    initial2

--- Stl.cpp
+++ Stl.cpp
@@ -1,1 +1,8 @@
 // we will see pre-defined keywords in this standard template library of cpp
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int> vec;
+    return 0;
+}
