cb6c72a80cb66d6bf463b23ad655993c48046824
Sonam Gupta <sonamflower34@gmail.com>
2026-03-30 18:05:38 +0000

    final

--- Stl.cpp
+++ Stl.cpp
@@ -17,5 +17,14 @@ int main(){
     }
     //vector<int> vec(3,10);
     //vector<int> vec2(vec1);
+
+
+//we can make iterator in vector
+vector<int> vec1 = {1,2,3,4,5};
+vector<int>::iterator it;
+for(it = vec1.begin(); it != vec1.end();it++){
+cout<<*(it)<<" ";
+}
+cout<<endl;
     return 0;
 }
