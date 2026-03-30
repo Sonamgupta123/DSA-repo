567c5648d697c06c0845a394f22274635805055f
Sonam Gupta <sonamflower34@gmail.com>
2026-03-30 17:41:03 +0000

    inital 5

--- Stl.cpp
+++ Stl.cpp
@@ -5,8 +5,12 @@ using namespace std;
 int main(){
     vector<int> vec;
     vec.push_back(1) ;
-    vec.push_back(2)
-    cout<<vec.size()<<endl;
-    cout<<vec.capacity()<<endl;
+    vec.push_back(2);
+    vec.push_back(3);
+    cout<<" size "<<vec.size()<<endl;
+    cout<<"capacity "<<vec.capacity()<<endl;
+    for(int val : vec){
+        cout<<val<<" ";
+    }
     return 0;
 }
