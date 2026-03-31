368f9b7214f4283bb102e75e6b81a1d125222280
Sonam Gupta <sonamflower34@gmail.com>
2026-03-31 17:43:36 +0000

    initial

--- List.cpp
+++ List.cpp
@@ -1,9 +1,12 @@
 #include<iostream>
 #include<vector>
 #include<list>
+#include<deque>
 using namespace std;
 int main(){
     list<int> l;
+    deque<int> d ={1,2,3};// random access possible like cout<<d[2]
+    // d.push_back(1);
     l.push_back(1);
      l.push_back(2);
       l.push_back(3);
