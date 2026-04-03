e3a8d757ca6778940baf692102634f587af12f1b
Sonam Gupta <sonamflower34@gmail.com>
2026-04-03 15:35:24 +0000

    adding 1

--- QuestionLeet.cpp
+++ QuestionLeet.cpp
@@ -4,59 +4,9 @@
 #include<climits>
 using namespace std;
 
-int maximumAmount(vector<vector<int>>& coins) {
-    int m = coins.size();
-    int n = coins[0].size();
 
-    vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(3, INT_MIN)));
-
-    dp[0][0][0] = coins[0][0];
-    if (coins[0][0] < 0) {
-        dp[0][0][1] = 0;
-    }
-
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            for (int k = 0; k <= 2; k++) {
-                if (dp[i][j][k] == INT_MIN) continue;
-
-                // Right
-                if (j + 1 < n) {
-                    int val = coins[i][j + 1];
-
-                    dp[i][j + 1][k] = max(dp[i][j + 1][k], dp[i][j][k] + val);
-
-                    if (val < 0 && k < 2) {
-                        dp[i][j + 1][k + 1] = max(dp[i][j + 1][k + 1], dp[i][j][k]);
-                    }
-                }
-
-                // Down
-                if (i + 1 < m) {
-                    int val = coins[i + 1][j];
-
-                    dp[i + 1][j][k] = max(dp[i + 1][j][k], dp[i][j][k] + val);
-
-                    if (val < 0 && k < 2) {
-                        dp[i + 1][j][k + 1] = max(dp[i + 1][j][k + 1], dp[i][j][k]);
-                    }
-                }
-            }
-        }
-    }
-
-    return max({dp[m-1][n-1][0], dp[m-1][n-1][1], dp[m-1][n-1][2]});
-}
 
 int main() {
-    vector<vector<int>> coins = {
-        {0, 1, -1},
-        {1, -2, 3},
-        {2, -3, 4}
-    };
-
-    int ans = maximumAmount(coins);
-    cout << ans;
-
+    
     return 0;
 }
