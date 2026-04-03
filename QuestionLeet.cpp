2450b53563608cdf2e72c4ca00ec474f432fd9af
Sonam Gupta <sonamflower34@gmail.com>
2026-04-03 16:00:13 +0000

    adding 2

--- QuestionLeet.cpp
+++ QuestionLeet.cpp
@@ -3,10 +3,14 @@
 #include<algorithm>
 #include<climits>
 using namespace std;
+int maxWalls(vector<int>& robots, vector<int>& distance, vector<int>& walls) {
 
+}
 
 
 int main() {
-    
+    vector<int> robots = {};
+    vector<int> distance = {};
+    vector<int> walls = {};
     return 0;
 }
