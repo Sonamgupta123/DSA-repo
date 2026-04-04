3f80d345208a17609cc6b90013dd034bebd90744
Sonam Gupta <sonamflower34@gmail.com>
2026-04-04 15:03:12 +0000

    add 2

--- QuestionLeet.cpp
+++ QuestionLeet.cpp
@@ -2,7 +2,12 @@
 
 using namespace std;
  string decodeCiphertext(string encodedText, int rows) {
-        
+        if(rows == 0) return "";
+
+        int n = encodedText.size();
+        int cols = n / rows;
+
+        vector<vector<char>> mat(rows, vector<char>(cols));
     }
 
 int main() {
