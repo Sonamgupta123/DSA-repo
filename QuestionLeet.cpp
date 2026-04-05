aee9c5827e94c0bac84555aedf642223284f4858
Sonam Gupta <sonamflower34@gmail.com>
2026-04-05 14:12:09 +0000

    done 1

--- QuestionLeet.cpp
+++ QuestionLeet.cpp
@@ -1,7 +1,17 @@
 #include<iostream>
 #include<vector>
 using namespace std;
- 
+ bool judgeCircle(string moves) {
+        int x = 0, y = 0;
+
+        for(char move : moves) {
+            if(move == 'U') y++;
+            else if(move == 'D') y--;
+            else if(move == 'R') x++;
+            else if(move == 'L') x--;
+        }
+
+        return (x == 0 && y == 0);
 
 int main() {
     
