e7c144667f5c693d5868f3c23c8109c0244a397d
Sonam Gupta <sonamflower34@gmail.com>
2026-04-06 13:50:11 +0000

    three

--- QuestionLeet.cpp
+++ QuestionLeet.cpp
@@ -1,7 +1,13 @@
 #include<iostream>
 #include<vector>
 using namespace std;
- 
+  int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
+        
+        set<pair<int,int>> obs;
+        for(auto &o : obstacles){
+            obs.insert({o[0], o[1]});
+        }
+        
 int main() {
   
     return 0;
