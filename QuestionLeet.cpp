76379a8ad313a8c78ac8f3551fa9375dae72540c
Sonam Gupta <sonamflower34@gmail.com>
2026-04-06 13:50:50 +0000

    four

--- QuestionLeet.cpp
+++ QuestionLeet.cpp
@@ -7,7 +7,13 @@ using namespace std;
         for(auto &o : obstacles){
             obs.insert({o[0], o[1]});
         }
-        
+         vector<pair<int,int>> dir = {
+            {0,1}, {1,0}, {0,-1}, {-1,0}
+        };
+        int x = 0, y = 0;
+        int d = 0; // start facing North
+        int maxDist = 0;
+
 int main() {
   
     return 0;
