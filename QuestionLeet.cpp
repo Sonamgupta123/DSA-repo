4da97088e0af8288f0210f50cfe9896b6f8bd21d
Sonam Gupta <sonamflower34@gmail.com>
2026-04-06 13:51:20 +0000

    five

--- QuestionLeet.cpp
+++ QuestionLeet.cpp
@@ -13,6 +13,30 @@ using namespace std;
         int x = 0, y = 0;
         int d = 0; // start facing North
         int maxDist = 0;
+        for(int cmd : commands){
+            if(cmd == -1){
+                d = (d + 1) % 4; // turn right
+            }
+            else if(cmd == -2){
+                d = (d + 3) % 4; // turn left
+            }
+            else{
+                for(int i = 0; i < cmd; i++){
+                    int nx = x + dir[d].first;
+                    int ny = y + dir[d].second;
+
+                    if(obs.count({nx, ny})) break;
+
+                    x = nx;
+                    y = ny;
+
+                    maxDist = max(maxDist, x*x + y*y);
+                }
+            }
+        }
+
+        return maxDist;
+    }
 
 int main() {
   
