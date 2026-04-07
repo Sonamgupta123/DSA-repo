e447c2bc0793fb8f6909a040cae0722b654d7b56
Sonam Gupta <sonamflower34@gmail.com>
2026-04-07 15:02:04 +0000

    abcdef

--- QuestionLeet.cpp
+++ QuestionLeet.cpp
@@ -8,7 +8,15 @@ private:
     int x, y;
     int dir; // 0=East, 1=North, 2=West, 3=South
     int cycle;
-
+public:
+    Robot(int width, int height) {
+        w = width;
+        h = height;
+        x = 0;
+        y = 0;
+        dir = 0; // East
+        cycle = 2 * (w + h) - 4;
+    }
 int main() {
    
 
