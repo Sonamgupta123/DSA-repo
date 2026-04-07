e098bf457f85dbf77ab565cd0f27e5d99ebefacd
Sonam Gupta <sonamflower34@gmail.com>
2026-04-07 15:03:23 +0000

    final

--- QuestionLeet.cpp
+++ QuestionLeet.cpp
@@ -6,8 +6,63 @@ using namespace std;
 int x = 0, y = 0;
 int dir = 0; // 0=East, 1=North, 2=West, 3=South
 int cycle;
+void step(int num) {
+    if (cycle == 0) return;
+
+    num %= cycle;
+    if (num == 0) num = cycle;
+
+    while (num--) {
+        if (dir == 0) { // East
+            if (x + 1 < w) x++;
+            else { dir = 1; y++; }
+        }
+        else if (dir == 1) { // North
+            if (y + 1 < h) y++;
+            else { dir = 2; x--; }
+        }
+        else if (dir == 2) { // West
+            if (x - 1 >= 0) x--;
+            else { dir = 3; y--; }
+        }
+        else { // South
+            if (y - 1 >= 0) y--;
+            else { dir = 0; x++; }
+        }
+    }
+}
+
+vector<int> getPos() {
+    return {x, y};
+}
+
+string getDir() {
+    if (dir == 0) return "East";
+    if (dir == 1) return "North";
+    if (dir == 2) return "West";
+    return "South";
+}
+
 int main() {
-   
+    // Example usage
+    w = 6; 
+    h = 3;
+    cycle = 2 * (w + h) - 4;
+
+    step(2);
+    step(2);
+
+    vector<int> pos = getPos();
+    cout << pos[0] << " " << pos[1] << endl; // 4 0
+    cout << getDir() << endl; // East
+
+    step(2);
+    step(1);
+    step(4);
+
+    pos = getPos();
+    cout << pos[0] << " " << pos[1] << endl; // 1 2
+    cout << getDir() << endl; // West
 
     return 0;
 }
