fed3b31b4ae23d2c75a0a1887de22e4df77e7089
Sonam Gupta <sonamflower34@gmail.com>
2026-04-07 14:58:39 +0000

    abcd

--- QuestionLeet.cpp
+++ QuestionLeet.cpp
@@ -5,10 +5,7 @@ using namespace std;
   
 
 int main() {
-   vector<int> commands = {4,-1,4,-2,4};
-  vector< vector<int>> obstacles = {{2,4}};
-  int ans = robotSim(commands,obstacles);
-  cout<<ans<<endl;
+   
 
     return 0;
 }
