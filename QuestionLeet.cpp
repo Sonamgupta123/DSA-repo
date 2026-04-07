a21adec1f4694a7e111d21c441ec126bcb162f39
Sonam Gupta <sonamflower34@gmail.com>
2026-04-07 15:02:48 +0000

    one

--- QuestionLeet.cpp
+++ QuestionLeet.cpp
@@ -2,21 +2,10 @@
 #include<vector>
 #include <bits/stdc++.h>
 using namespace std;
-  class Robot {
-private:
-    int w, h;
-    int x, y;
-    int dir; // 0=East, 1=North, 2=West, 3=South
-    int cycle;
-public:
-    Robot(int width, int height) {
-        w = width;
-        h = height;
-        x = 0;
-        y = 0;
-        dir = 0; // East
-        cycle = 2 * (w + h) - 4;
-    }
+ int w, h;
+int x = 0, y = 0;
+int dir = 0; // 0=East, 1=North, 2=West, 3=South
+int cycle;
 int main() {
    
 
