fc216bbe97b7954f23855a31967ce3359822a8de
Sonam Gupta <sonamflower34@gmail.com>
2026-04-07 15:07:52 +0000

    one4

--- QuestionLeet2.cpp
+++ QuestionLeet2.cpp
@@ -10,6 +10,37 @@ void step(int num) {
 
     num %= cycle;
     if (num == 0) num = cycle;
+     while (num--) {
+        if (dir == 0) { // East
+            if (x + 1 < w) x++;
+            else { dir = 1; y++; }
+        }
+        else if (dir == 1) { // North
+            if (y + 1 < h) y++;
+            else { dir = 2; x--; }
+        }
+        else if (dir == 2) { // West
+            if (x - 1 >= 0) x--;
+            else { dir = 3; y--; }
+        }
+        else { // South
+            if (y - 1 >= 0) y--;
+            else { dir = 0; x++; }
+        }
+    }
+}
+
+vector<int> getPos() {
+    return {x, y};
+}
+
+string getDir() {
+    if (dir == 0) return "East";
+    if (dir == 1) return "North";
+    if (dir == 2) return "West";
+    return "South";
+}
+
 int main(){
     return 0;
 }
