9f64ee6a7dcfa136ac9d457580d0dbb652dd7f1a
Sonam Gupta <sonamflower34@gmail.com>
2026-04-07 15:01:14 +0000

    abcde

--- QuestionLeet.cpp
+++ QuestionLeet.cpp
@@ -1,8 +1,13 @@
 #include<iostream>
 #include<vector>
-
+#include <bits/stdc++.h>
 using namespace std;
-  
+  class Robot {
+private:
+    int w, h;
+    int x, y;
+    int dir; // 0=East, 1=North, 2=West, 3=South
+    int cycle;
 
 int main() {
    
