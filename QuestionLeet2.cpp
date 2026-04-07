5f897025aa942e2f6a753719760ccef46a8fe7ce
Sonam Gupta <sonamflower34@gmail.com>
2026-04-07 15:07:18 +0000

    one3

--- QuestionLeet2.cpp
+++ QuestionLeet2.cpp
@@ -1,6 +1,15 @@
 #include<iostream>
 #include<vector>
 using namespace std;
+int w, h;
+int x = 0, y = 0;
+int dir = 0; // 0=East, 1=North, 2=West, 3=South
+int cycle;
+void step(int num) {
+    if (cycle == 0) return;
+
+    num %= cycle;
+    if (num == 0) num = cycle;
 int main(){
     return 0;
 }
