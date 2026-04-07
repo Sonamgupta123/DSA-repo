bee2712dafcac58145b26b4f33eb97a3b4fa40ac
Sonam Gupta <sonamflower34@gmail.com>
2026-04-07 14:58:18 +0000

    abc

--- QuestionLeet.cpp
+++ QuestionLeet.cpp
@@ -1,44 +1,8 @@
 #include<iostream>
 #include<vector>
-#include<set>
-#include<algorithm>
+
 using namespace std;
-  int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
-        
-        set<pair<int,int>> obs;
-        for(auto &o : obstacles){
-            obs.insert({o[0], o[1]});
-        }
-         vector<pair<int,int>> dir = {
-            {0,1}, {1,0}, {0,-1}, {-1,0}
-        };
-        int x = 0, y = 0;
-        int d = 0; // start facing North
-        int maxDist = 0;
-        for(int cmd : commands){
-            if(cmd == -1){
-                d = (d + 1) % 4; // turn right
-            }
-            else if(cmd == -2){
-                d = (d + 3) % 4; // turn left
-            }
-            else{
-                for(int i = 0; i < cmd; i++){
-                    int nx = x + dir[d].first;
-                    int ny = y + dir[d].second;
-
-                    if(obs.count({nx, ny})) break;
-
-                    x = nx;
-                    y = ny;
-
-                    maxDist = max(maxDist, x*x + y*y);
-                }
-            }
-        }
-
-        return maxDist;
-    }
+  
 
 int main() {
    vector<int> commands = {4,-1,4,-2,4};
