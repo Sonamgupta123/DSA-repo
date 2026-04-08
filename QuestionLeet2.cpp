421daf5215af4383ca1f902dc4044ba621560e1b
Sonam Gupta <sonamflower34@gmail.com>
2026-04-08 16:19:41 +0000

    test2

--- QuestionLeet2.cpp
+++ QuestionLeet2.cpp
@@ -4,22 +4,6 @@ using namespace std;
 
 
 int main(){
-      w = 6; 
-    h = 3;
-    cycle = 2 * (w + h) - 4;
-
-    step(2);
-    step(2);
-
-    vector<int> pos = getPos();
-    cout << pos[0] << " " << pos[1] << endl; // 4 0
-    cout << getDir() << endl; // East
- step(2);
-    step(1);
-    step(4);
-
-    pos = getPos();
-    cout << pos[0] << " " << pos[1] << endl; // 1 2
-    cout << getDir() << endl; // West
+   
     return 0;
 }
