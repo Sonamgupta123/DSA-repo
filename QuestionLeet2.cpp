ee47640bf8baf79d6ea6f5f2ccc5eb2d3bb0cf0f
Sonam Gupta <sonamflower34@gmail.com>
2026-04-08 16:17:51 +0000

    test

--- QuestionLeet2.cpp
+++ QuestionLeet2.cpp
@@ -1,45 +1,7 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-int w, h;
-int x = 0, y = 0;
-int dir = 0; // 0=East, 1=North, 2=West, 3=South
-int cycle;
-void step(int num) {
-    if (cycle == 0) return;
 
-    num %= cycle;
-    if (num == 0) num = cycle;
-     while (num--) {
-        if (dir == 0) { // East
-            if (x + 1 < w) x++;
-            else { dir = 1; y++; }
-        }
-        else if (dir == 1) { // North
-            if (y + 1 < h) y++;
-            else { dir = 2; x--; }
-        }
-        else if (dir == 2) { // West
-            if (x - 1 >= 0) x--;
-            else { dir = 3; y--; }
-        }
-        else { // South
-            if (y - 1 >= 0) y--;
-            else { dir = 0; x++; }
-        }
-    }
-}
-
-vector<int> getPos() {
-    return {x, y};
-}
-
-string getDir() {
-    if (dir == 0) return "East";
-    if (dir == 1) return "North";
-    if (dir == 2) return "West";
-    return "South";
-}
 
 int main(){
       w = 6; 
