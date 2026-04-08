e093b5c41d00d1aa6f87989b2c5614a6e01c6bf0
Sonam Gupta <sonamflower34@gmail.com>
2026-04-08 16:20:10 +0000

    test3

--- QuestionLeet2.cpp
+++ QuestionLeet2.cpp
@@ -1,7 +1,16 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-
+ int xorAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
+        const int MOD = 1e9 + 7;
+        
+        for (auto &q : queries) {
+            int l = q[0], r = q[1], k = q[2], v = q[3];
+            
+            for (int i = l; i <= r; i += k) {
+                nums[i] = (1LL * nums[i] * v) % MOD;
+            }
+        }
 
 int main(){
    
