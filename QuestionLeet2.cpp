0fe94ca4df9890a00687dae1a9b7f5cecb8d33b5
Sonam Gupta <sonamflower34@gmail.com>
2026-04-09 13:31:50 +0000

    final

--- QuestionLeet2.cpp
+++ QuestionLeet2.cpp
@@ -74,7 +74,7 @@ using namespace std;
 int main(){
    vector<int> nums = {2,3,1,5,4};
    vector<vector<int>> queries = {{1,4,2,3},{0,2,1,2}};
-   int ans = xorAfterQueries(nums , queries);
-   cout<<ans<<endl;
+   int result = xorAfterQueries(nums , queries);
+   cout<<result<<endl;
     return 0;
 }
