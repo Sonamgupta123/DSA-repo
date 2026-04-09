0a1daf23c125b36022409c449e37acf26ec3d5be
Sonam Gupta <sonamflower34@gmail.com>
2026-04-09 13:29:14 +0000

    add1

--- QuestionLeet2.cpp
+++ QuestionLeet2.cpp
@@ -1,7 +1,13 @@
 #include<iostream>
 #include<vector>
 using namespace std;
- 
+  long long power(long long a, long long b){
+        if(b==0) return 1;
+        long long half = power(a,b/2);
+        long long result = (half*half)%M;
+        if(b%2) result = (result * a) % M;
+        return result;
+    }
 int main(){
    vector<int> nums = {2,3,1,5,4};
    vector<vector<int>> queries = {{1,4,2,3},{0,2,1,2}};
