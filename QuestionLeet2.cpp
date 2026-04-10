3679eb32fbc7fc1e8ea1a9c00b926cae42403fe4
Sonam Gupta <sonamflower34@gmail.com>
2026-04-10 15:19:37 +0000

    adde

--- QuestionLeet2.cpp
+++ QuestionLeet2.cpp
@@ -10,9 +10,6 @@ using namespace std;
         
 
 int main(){
-   vector<int> nums = {2,3,1,5,4};
-   vector<vector<int>> queries = {{1,4,2,3},{0,2,1,2}};
-   int result = xorAfterQueries(nums , queries);
-   cout<<result<<endl;
+  
     return 0;
 }
