15ce5c78ddda565a50f774031b35d389bfc69071
Sonam Gupta <sonamflower34@gmail.com>
2026-04-12 14:25:36 +0000

    innnm

--- QuestionLeet2.cpp
+++ QuestionLeet2.cpp
@@ -8,6 +8,44 @@ int dist(int a, int b) {
         if (a == 26) return 0; // finger not used yet
         return abs(a / 6 - b / 6) + abs(a % 6 - b % 6);
     }
+     int minimumDistance(string word) {
+        int n = word.size();
+        
+        // dp[l][r] = min cost
+        vector<vector<int>> dp(27, vector<int>(27, INT_MAX));
+        dp[26][26] = 0; // both fingers unused
+        
+        for (char ch : word) {
+            int curr = ch - 'A';
+            
+            vector<vector<int>> newdp(27, vector<int>(27, INT_MAX));
+            
+            for (int l = 0; l <= 26; l++) {
+                for (int r = 0; r <= 26; r++) {
+                    if (dp[l][r] == INT_MAX) continue;
+                    
+                    // move left finger
+                    newdp[curr][r] = min(newdp[curr][r],
+                                         dp[l][r] + dist(l, curr));
+                    
+                    // move right finger
+                    newdp[l][curr] = min(newdp[l][curr],
+                                         dp[l][r] + dist(r, curr));
+                }
+            }
+            
+            dp = newdp;
+        }
+        
+        int ans = INT_MAX;
+        for (int l = 0; l <= 26; l++) {
+            for (int r = 0; r <= 26; r++) {
+                ans = min(ans, dp[l][r]);
+            }
+        }
+        
+        return ans;
+    }
    
 int main(){
  
