8a36a74143641cf74c2c3b0e739eda35e30cf4af
Sonam Gupta <sonamflower34@gmail.com>
2026-04-12 14:24:33 +0000

    inn

--- QuestionLeet2.cpp
+++ QuestionLeet2.cpp
@@ -4,29 +4,8 @@
 #include<deque>
 #include <unordered_map>
 using namespace std;
-    int minimumDistance(vector<int>& nums) {
-        unordered_map<int, deque<int>> mp;
-        int ans = INT_MAX;
-        
-        for (int i = 0; i < nums.size(); i++) {
-            int val = nums[i];
-            mp[val].push_back(i);
-            
-            // Keep only last 3 indices
-            if (mp[val].size() > 3) {
-                mp[val].pop_front();
-            }
-              if (mp[val].size() == 3) {
-                int dist = 2 * (mp[val].back() - mp[val].front());
-                ans = min(ans, dist);
-            }
-        }
-        
-        return (ans == INT_MAX) ? -1 : ans;
-    }
+   
 int main(){
- vector<int> nums = {1,2,1,1,3};
- int ans = minimumDistance(nums);
- cout<<ans<<endl;
+ 
     return 0;
 }
