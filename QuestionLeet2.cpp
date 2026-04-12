488ce4bdd73bcbe67a41af7fead8709e35a9548d
Sonam Gupta <sonamflower34@gmail.com>
2026-04-12 14:25:10 +0000

    innn

--- QuestionLeet2.cpp
+++ QuestionLeet2.cpp
@@ -4,6 +4,10 @@
 #include<deque>
 #include <unordered_map>
 using namespace std;
+int dist(int a, int b) {
+        if (a == 26) return 0; // finger not used yet
+        return abs(a / 6 - b / 6) + abs(a % 6 - b % 6);
+    }
    
 int main(){
  
