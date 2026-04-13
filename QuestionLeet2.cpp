67ad8c5694924f907766d9c3e745a2e6abe6e6ab
Sonam Gupta <sonamflower34@gmail.com>
2026-04-13 15:39:40 +0000

    rem2

--- QuestionLeet2.cpp
+++ QuestionLeet2.cpp
@@ -5,19 +5,6 @@
 #include <unordered_map>
 using namespace std;
 
-        
-       
-          
-        
-        int ans = INT_MAX;
-        for (int l = 0; l <= 26; l++) {
-            for (int r = 0; r <= 26; r++) {
-                ans = min(ans, dp[l][r]);
-            }
-        }
-        
-        return ans;
-    }
    
 int main(){
   string word = "HAPPY";
