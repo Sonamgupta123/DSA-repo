ace0e1ff873f47a3b3fdda893c8bcb668c7ac99a
Sonam Gupta <sonamflower34@gmail.com>
2026-04-13 15:43:41 +0000

    final

--- QuestionLeet2.cpp
+++ QuestionLeet2.cpp
@@ -17,9 +17,9 @@ using namespace std;
     }
    
 int main(){
- vector<int> nums = {};
- int target = ;
- int start = ;
+ vector<int> nums = {1,2,3,4,5};
+ int target = 5;
+ int start = 3;
  int ans =getMinDistance(nums, target,start);
  cout<<ans<<endl;
     return 0;
