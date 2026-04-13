0eab9dbd6150e661e10a6367c0b2ef6596f01648
Sonam Gupta <sonamflower34@gmail.com>
2026-04-13 15:38:49 +0000

    rem

--- QuestionLeet2.cpp
+++ QuestionLeet2.cpp
@@ -4,21 +4,9 @@
 #include<deque>
 #include <unordered_map>
 using namespace std;
-int dist(int a, int b) {
-        if (a == 26) return 0; // finger not used yet
-        return abs(a / 6 - b / 6) + abs(a % 6 - b % 6);
-    }
-     int minimumDistance(string word) {
-        int n = word.size();
+
         
-        // dp[l][r] = min cost
-        vector<vector<int>> dp(27, vector<int>(27, INT_MAX));
-        dp[26][26] = 0; // both fingers unused
-        
-        for (char ch : word) {
-            int curr = ch - 'A';
-            
-            vector<vector<int>> newdp(27, vector<int>(27, INT_MAX));
+       
             
             for (int l = 0; l <= 26; l++) {
                 for (int r = 0; r <= 26; r++) {
