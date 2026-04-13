487b39ec7d60e039858e66c119f7ed3c7bccd2a0
Sonam Gupta <sonamflower34@gmail.com>
2026-04-13 15:41:23 +0000

    rem4

--- QuestionLeet2.cpp
+++ QuestionLeet2.cpp
@@ -4,7 +4,17 @@
 #include<deque>
 #include <unordered_map>
 using namespace std;
-
+ int getMinDistance(vector<int>& nums, int target, int start) {
+        int ans = INT_MAX;
+        
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] == target) {
+                ans = min(ans, abs(i - start));
+            }
+        }
+        
+        return ans;
+    }
    
 int main(){
  
