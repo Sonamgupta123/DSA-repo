a21d769ad4c90f96afa5e3a94f414fb622f9603f
Sonam Gupta <sonamflower34@gmail.com>
2026-04-14 17:16:51 +0000

    one1

--- QuestionLeet2.cpp
+++ QuestionLeet2.cpp
@@ -12,7 +12,18 @@ long long minimumTotalDistance(vector<int>& robot, vector<vector<int>>& factory)
         int m = factory.size();
         
         vector<vector<long long>> dp(n + 1, vector<long long>(m + 1, -1));
-
+ function<long long(int, int)> solve = [&](int i, int j) -> long long {
+            // all robots assigned
+            if (i == n) return 0;
+            
+            // no factories left
+            if (j == m) return 1e18;
+            
+            if (dp[i][j] != -1) return dp[i][j];
+            
+            long long res = solve(i, j + 1); // skip this factory
+            
+            long long cost = 0;
    
 int main(){
  
