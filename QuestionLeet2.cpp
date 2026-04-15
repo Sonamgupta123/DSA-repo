4016c8fa132bf7054038858f672059712527dece
Sonam Gupta <sonamflower34@gmail.com>
2026-04-15 16:58:52 +0000

    final

--- QuestionLeet2.cpp
+++ QuestionLeet2.cpp
@@ -24,5 +24,8 @@ int main(){
  vector<string> words = {"a","b","leetcode"};
  string target = "leetcode";
  int startIndex = 0 ;
+ int ans = closestTarget(words, target,startIndex);
+ cout<<ans<<endl;
+ 
     return 0;
 }
