5755da7eeedf98a1cd58ac973fb3e1a605ef0133
Sonam Gupta <sonamflower34@gmail.com>
2026-04-15 16:54:37 +0000

    rem2

--- QuestionLeet2.cpp
+++ QuestionLeet2.cpp
@@ -10,9 +10,6 @@ using namespace std;
        
    
 int main(){
- vector<int> robot = {1,-1};
- vector<vector<int>> factory = {{-2,1},{2,1}};
- long long ans = minimumTotalDistance(robot,factory);
- cout<<ans<<endl;
+ 
     return 0;
 }
