c981a3f7da9a6340ed150abc4cdc4c76a1d98aec
Sonam Gupta <sonamflower34@gmail.com>
2026-04-15 16:54:24 +0000

    rem1

--- QuestionLeet2.cpp
+++ QuestionLeet2.cpp
@@ -7,30 +7,7 @@
 using namespace std;
 
         
-        vector<vector<long long>> dp(n + 1, vector<long long>(m + 1, -1));
- function<long long(int, int)> solve = [&](int i, int j) -> long long {
-            // all robots assigned
-            if (i == n) return 0;
-            
-            // no factories left
-            if (j == m) return 1e18;
-            
-            if (dp[i][j] != -1) return dp[i][j];
-            
-            long long res = solve(i, j + 1); // skip this factory
-            
-            long long cost = 0;
-              // try assigning k robots to this factory
-            for (int k = 0; k < factory[j][1] && i + k < n; k++) {
-                cost += abs(robot[i + k] - factory[j][0]);
-                res = min(res, cost + solve(i + k + 1, j + 1));
-            }
-            
-            return dp[i][j] = res;
-        };
-        
-        return solve(0, 0);
-    }
+       
    
 int main(){
  vector<int> robot = {1,-1};
