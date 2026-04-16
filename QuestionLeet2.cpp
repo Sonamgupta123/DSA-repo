f2126715a5eab73425234828b86b2c61aa43af95
Sonam Gupta <sonamflower34@gmail.com>
2026-04-16 15:45:28 +0000

    he

--- QuestionLeet2.cpp
+++ QuestionLeet2.cpp
@@ -5,7 +5,14 @@
 #include<deque>
 #include <unordered_map>
 using namespace std;
- 
+  vector<int> solveQueries(vector<int>& nums, vector<int>& queries) {
+        int n = nums.size();
+        
+        // Step 1: value -> indices mapping
+        unordered_map<int, vector<int>> mp;
+        for (int i = 0; i < n; i++) {
+            mp[nums[i]].push_back(i);
+        }
        
    
 int main(){
