099b6c43e3e0f3668cb82e72bf295568e2fc94a5
Sonam Gupta <sonamflower34@gmail.com>
2026-04-17 15:45:41 +0000

    add3

--- QuestionLeet2.cpp
+++ QuestionLeet2.cpp
@@ -11,11 +11,7 @@ using namespace std;
        
    
 int main(){
- vector<int> nums = {1,2,3,4};
- vector<int> queries = {0,1,2,3};
- vector<int> ans = solveQueries(nums , queries);
- for( int x : ans){
-    cout<<x<<endl;
+    
+  return 0;
  }
-    return 0;
-}
+   
