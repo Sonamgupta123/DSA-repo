fb398533ff144f2bfdcf1a300db225643a772a8a
Sonam Gupta <sonamflower34@gmail.com>
2026-04-17 15:47:40 +0000

    add4

--- QuestionLeet2.cpp
+++ QuestionLeet2.cpp
@@ -6,12 +6,17 @@
 #include <unordered_map>
 using namespace std;
  
-        
+    int minMirrorPairDistance(vector<int>& nums) {
+        unordered_map<int, int> mp; // number -> latest index
+        int ans = INT_MAX;
+
+        for (int j = 0; j < nums.size(); j++) {
+            int x = nums[j];     
        
        
    
 int main(){
-    
+
   return 0;
  }
    
