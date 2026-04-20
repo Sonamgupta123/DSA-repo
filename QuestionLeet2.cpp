b4925356b25d601213033bbfe5994abe7c489c0c
Sonam Gupta <sonamflower34@gmail.com>
2026-04-20 16:38:48 +0000

    one1

--- QuestionLeet2.cpp
+++ QuestionLeet2.cpp
@@ -8,10 +8,7 @@ using namespace std;
  
   
 int main(){
- vector<int> nums1 = {30,29,19,5};
- vector<int> nums2 = {25,25,25,25,25};
- int ans = maxDistance(nums1 , nums2);
- cout<<ans<<endl;
+ 
   return 0;
  }
    
