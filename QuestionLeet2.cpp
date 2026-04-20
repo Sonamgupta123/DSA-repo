41058dc0ff14ebba5e371e05f1057110d4172c3e
Sonam Gupta <sonamflower34@gmail.com>
2026-04-20 16:38:24 +0000

    one

--- QuestionLeet2.cpp
+++ QuestionLeet2.cpp
@@ -5,21 +5,7 @@
 #include<deque>
 #include <unordered_map>
 using namespace std;
- int maxDistance(vector<int>& nums1, vector<int>& nums2) {
-        int i = 0, j = 0;
-        int ans = 0;
-
-        while (i < nums1.size() && j < nums2.size()) {
-            if (nums1[i] <= nums2[j]) {
-                ans = max(ans, j - i);
-                j++;  // try to increase distance
-            } else {
-                i++;  // move i to satisfy condition
-            }
-        }
-
-        return ans;
-    }
+ 
   
 int main(){
  vector<int> nums1 = {30,29,19,5};
