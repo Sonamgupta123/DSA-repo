c795a6405ef3bd4c316903a8fb905da0a56e0e45
Sonam Gupta <sonamflower34@gmail.com>
2026-04-21 11:39:50 +0000

    one

--- QuestionLeet2.cpp
+++ QuestionLeet2.cpp
@@ -5,23 +5,7 @@
 #include<deque>
 #include <unordered_map>
 using namespace std;
-  int maxDistance(vector<int>& colors) {
-        int n = colors.size();
-
-        for (int i = n - 1; i >= 0; i--) {
-            if (colors[i] != colors[0]) {
-                return i; // max distance from left
-            }
-        }
-
-        for (int i = 0; i < n; i++) {
-            if (colors[i] != colors[n - 1]) {
-                return (n - 1 - i); // max distance from right
-            }
-        }
-
-        return 0; 
-    }
+ 
   
 int main(){
  vector<int> colors = {0,1};
