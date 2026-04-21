e33782545d1e1825223475a8e032b530c8ae718c
Sonam Gupta <sonamflower34@gmail.com>
2026-04-21 11:41:26 +0000

    two2

--- QuestionLeet2.cpp
+++ QuestionLeet2.cpp
@@ -15,6 +15,21 @@ using namespace std;
         // Step 1: Union
         for (auto &e : allowedSwaps)
             unite(e[0], e[1]);
+          // Step 2: Group indices
+        unordered_map<int, vector<int>> groups;
+        for (int i = 0; i < n; i++) {
+            groups[find(i)].push_back(i);
+        }
+
+        int ans = 0;
+
+        // Step 3: Process each group
+        for (auto &g : groups) {
+            unordered_map<int, int> freq;
+
+            // Count source values
+            for (int idx : g.second)
+                freq[source[idx]]++;
   
 int main(){
  
