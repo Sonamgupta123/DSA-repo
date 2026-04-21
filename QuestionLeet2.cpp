714a5d7531a559f41bcac62f1655bd7086b377c2
Sonam Gupta <sonamflower34@gmail.com>
2026-04-21 11:41:04 +0000

    two1

--- QuestionLeet2.cpp
+++ QuestionLeet2.cpp
@@ -5,7 +5,16 @@
 #include<deque>
 #include <unordered_map>
 using namespace std;
- 
+   int minimumHammingDistance(vector<int>& source, vector<int>& target, vector<vector<int>>& allowedSwaps) {
+        int n = source.size();
+        parent.resize(n);
+
+        for (int i = 0; i < n; i++)
+            parent[i] = i;
+
+        // Step 1: Union
+        for (auto &e : allowedSwaps)
+            unite(e[0], e[1]);
   
 int main(){
  
