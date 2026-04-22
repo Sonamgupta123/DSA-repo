873f00500a8d8eae78348efb2d44a4223ce8f28a
Sonam Gupta <sonamflower34@gmail.com>
2026-04-22 15:15:53 +0000

    add2

--- QuestionLeet2.cpp
+++ QuestionLeet2.cpp
@@ -11,11 +11,7 @@ using namespace std;
   
   
 int main(){
- vector<int> source = {1,2,3,4};
- vector<int> target = {1,3,2,4};
- vector<vector<int>> allowedSwaps = {};
- int ans = minimumHammingDistance(source,target,allowedSwaps);
- cout<<ans<<endl; 
+ 
   return 0;
  }
    
