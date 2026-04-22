407cea994bc4f42b361a788f27fa3361206b2766
Sonam Gupta <sonamflower34@gmail.com>
2026-04-22 15:15:41 +0000

    add1

--- QuestionLeet2.cpp
+++ QuestionLeet2.cpp
@@ -8,43 +8,7 @@ using namespace std;
 
 
    
-   int minimumHammingDistance(vector<int>& source, vector<int>& target, vector<vector<int>>& allowedSwaps) {
-        int n = source.size();
-        parent.resize(n);
-
-        for (int i = 0; i < n; i++)
-            parent[i] = i;
-
-        // Step 1: Union
-        for (auto &e : allowedSwaps)
-            unite(e[0], e[1]);
-          // Step 2: Group indices
-        unordered_map<int, vector<int>> groups;
-        for (int i = 0; i < n; i++) {
-            groups[find(i)].push_back(i);
-        }
-
-        int ans = 0;
-
-        // Step 3: Process each group
-        for (auto &g : groups) {
-            unordered_map<int, int> freq;
-
-            // Count source values
-            for (int idx : g.second)
-                freq[source[idx]]++;
-             // Match target values
-            for (int idx : g.second) {
-                if (freq[target[idx]] > 0) {
-                    freq[target[idx]]--;
-                } else {
-                    ans++; // mismatch
-                }
-            }
-        }
-
-        return ans;
-    }
+  
   
 int main(){
  vector<int> source = {1,2,3,4};
