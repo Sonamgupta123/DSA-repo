dc9d0aea148e61f3b5f8c6cedc2d5a244748cf6e
Sonam Gupta <sonamflower34@gmail.com>
2026-04-23 15:15:27 +0000

    3

--- QuestionLeet2.cpp
+++ QuestionLeet2.cpp
@@ -7,22 +7,6 @@
 using namespace std;
 
  
-                    
-                    if (diff > 2) break; // early stop
-                }
-                
-                if (diff <= 2) {
-                    result.push_back(q);
-                    break; // no need to check further dictionary words
-                }
-            }
-        }
-        
-        return result;
-    }
-   
-  
-  
 int main(){
 
   return 0;
