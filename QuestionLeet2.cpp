60446959aa87da90c5c3657407af742811a7d4d1
Sonam Gupta <sonamflower34@gmail.com>
2026-04-23 15:19:47 +0000

    4

--- QuestionLeet2.cpp
+++ QuestionLeet2.cpp
@@ -5,7 +5,16 @@
 #include<deque>
 #include <unordered_map>
 using namespace std;
-
+vector<long long> distance(vector<int>& nums) {
+        int n = nums.size();
+        vector<long long> ans(n, 0);
+        
+        unordered_map<int, vector<int>> mp;
+        
+        // Step 1: store indices
+        for(int i = 0; i < n; i++) {
+            mp[nums[i]].push_back(i);
+        }
  
 int main(){
 
