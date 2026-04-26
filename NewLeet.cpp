03da5e678cb87337db5be7cf1219b5168807afb4
Sonam Gupta <sonamflower34@gmail.com>
2026-04-26 16:08:21 +0000

    add2

--- NewLeet.cpp
+++ NewLeet.cpp
@@ -10,11 +10,7 @@ using namespace std;
        
     
 int main(){
-int side = 2 ;
-vector<vector<int>> points = {{0,0},{0,1},{0,2},{1,2},{2,0},{2,2},{2,1}};
-int k = 5;
-int ans = maxDistance(side,points,k);
-cout<<ans<<endl;
+
   return 0;
  }
    
