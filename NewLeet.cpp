dc09560172d64781b9ac434925cbeed55bbf9155
Sonam Gupta <sonamflower34@gmail.com>
2026-04-26 16:15:04 +0000

    final

--- NewLeet.cpp
+++ NewLeet.cpp
@@ -1,66 +1,66 @@
 #include<iostream>
 #include<vector>
-#include<climits>
-#include<functional>
-#include<deque>
-#include <unordered_map>
 using namespace std;
 
- int m, n;
+int m, n;
+
+bool dfs(vector<vector<char>>& grid, vector<vector<bool>>& visited,
+         int x, int y, int px, int py) {
     
-    bool dfs(vector<vector<char>>& grid, vector<vector<bool>>& visited,
-             int x, int y, int px, int py) {
+    visited[x][y] = true;
+    
+    int dirs[4][2] = {{0,1},{1,0},{0,-1},{-1,0}};
+    
+    for (auto& d : dirs) {
+        int nx = x + d[0];
+        int ny = y + d[1];
         
-        visited[x][y] = true;
+        if (nx < 0 || ny < 0 || nx >= m || ny >= n)
+            continue;
         
-        int dirs[4][2] = {{0,1},{1,0},{0,-1},{-1,0}};
+        if (grid[nx][ny] != grid[x][y])
+            continue;
         
-        for (auto& d : dirs) {
-            int nx = x + d[0];
-            int ny = y + d[1];
-            
-            if (nx < 0 || ny < 0 || nx >= m || ny >= n)
-                continue;
-            
-            if (grid[nx][ny] != grid[x][y])
-                continue;
-            
-            // If not visited → continue DFS
-            if (!visited[nx][ny]) {
-                if (dfs(grid, visited, nx, ny, x, y))
+        if (!visited[nx][ny]) {
+            if (dfs(grid, visited, nx, ny, x, y))
+                return true;
+        }
+        else if (nx != px || ny != py) {
+            return true;
+        }
+    }
+    
+    return false;
+}
+
+bool containsCycle(vector<vector<char>>& grid) {
+    m = grid.size();
+    n = grid[0].size();
+    
+    vector<vector<bool>> visited(m, vector<bool>(n, false));
+    
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            if (!visited[i][j]) {
+                if (dfs(grid, visited, i, j, -1, -1))
                     return true;
             }
-            // If visited AND not parent → cycle found
-            else if (nx != px || ny != py) {
-                return true;
-            }
         }
-        
-        return false;
     }
     
-    bool containsCycle(vector<vector<char>>& grid) {
-        m = grid.size();
-        n = grid[0].size();
-        
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (!visited[i][j]) {
-                    if (dfs(grid, visited, i, j, -1, -1))
-                        return true;
-                }
-            }
-        }
-        
-        return false;
-    }
-       
-    
+    return false;
+}
+
 int main(){
- vector<vector<char>> grid = {{"a","b","b"},{"b","z","b"},{"b","b","a"}};
- bool ans = containsCycle(grid);
-  return 0;
- }
-   
+    vector<vector<char>> grid = {
+        {'a','b','b'},
+        {'b','z','b'},
+        {'b','b','a'}
+    };
+
+    bool ans = containsCycle(grid);
+    
+    cout << (ans ? "true" : "false") << endl;
+
+    return 0;
+}
