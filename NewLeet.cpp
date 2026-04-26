367b6dd9fd2f5265091f7a8881e134138bb01e7f
Sonam Gupta <sonamflower34@gmail.com>
2026-04-26 16:07:39 +0000

    add

--- NewLeet.cpp
+++ NewLeet.cpp
@@ -5,41 +5,7 @@
 #include<deque>
 #include <unordered_map>
 using namespace std;
-bool canPlace(vector<long long>& pos, int k, long long dist, long long per) {
-        int n = pos.size();
 
-        vector<long long> ext(2 * n);
-        for (int i = 0; i < n; i++) {
-            ext[i] = pos[i];
-            ext[i + n] = pos[i] + per;
-        }
-
-        for (int i = 0; i < n; i++) {
-            long long last = ext[i];
-            int count = 1;
-            int idx = i;
-
-            while (count < k) {
-                long long target = last + dist;
-                auto it = lower_bound(ext.begin() + idx + 1, ext.begin() + i + n, target);
-
-                if (it == ext.begin() + i + n) break;
-
-                idx = it - ext.begin();
-                last = ext[idx];
-                count++;
-            }
-              if (count == k) {
-                if (ext[idx] - ext[i] <= per - dist) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-  int maxDistance(int side, vector<vector<int>>& points, int k) {
-        vector<long long> pos;
-        long long per = 4LL * side;
 
         // mapping
         for (auto &p : points) {
