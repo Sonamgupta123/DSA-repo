bd6d572f0817708ad374c8cf539c3e30cb82dc43
Sonam Gupta <sonamflower34@gmail.com>
2026-04-27 16:35:23 +0000

    solve3

--- NewLeet.cpp
+++ NewLeet.cpp
@@ -23,7 +23,33 @@ using namespace std;
             }
             return false;
         };
-
+ 
+        queue<pair<int,int>> q;
+        vector<vector<bool>> vis(m, vector<bool>(n, false));
+        
+        q.push({0,0});
+        vis[0][0] = true;
+        
+        while (!q.empty()) {
+            auto [x, y] = q.front(); q.pop();
+            
+            if (x == m-1 && y == n-1) return true;
+            
+            for (auto &d : dirs[grid[x][y]]) {
+                int nx = x + d[0];
+                int ny = y + d[1];
+                
+                if (nx >= 0 && ny >= 0 && nx < m && ny < n && !vis[nx][ny]) {
+                    if (isValid(nx, ny, x, y)) {
+                        vis[nx][ny] = true;
+                        q.push({nx, ny});
+                    }
+                }
+            }
+        }
+        
+        return false;
+    }
 
 int main(){
    
