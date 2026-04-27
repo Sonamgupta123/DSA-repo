8cd51b93d8658eb2917673026323712b7e7d7060
Sonam Gupta <sonamflower34@gmail.com>
2026-04-27 16:34:12 +0000

    solve1

--- NewLeet.cpp
+++ NewLeet.cpp
@@ -5,15 +5,7 @@ using namespace std;
 
 
 int main(){
-    vector<vector<char>> grid = {
-        {'a','b','b'},
-        {'b','z','b'},
-        {'b','b','a'}
-    };
-
-    bool ans = containsCycle(grid);
-    
-    cout << (ans ? "true" : "false") << endl;
+   
 
     return 0;
 }
