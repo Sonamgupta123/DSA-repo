98ef320d95ffe85d65efa65fb28672c917612f14
Sonam Gupta <sonamflower34@gmail.com>
2026-04-28 15:32:32 +0000

    add3

--- NewLeet.cpp
+++ NewLeet.cpp
@@ -2,7 +2,21 @@
 #include<queue>
 #include<vector>
 using namespace std;
- 
+  int minOperations(vector<vector<int>>& grid, int x) {
+        vector<int> nums;
+        
+        // Step 1: Flatten grid
+        for (auto &row : grid) {
+            for (int val : row) {
+                nums.push_back(val);
+            }
+        }
+        
+        // Step 2: Check feasibility
+        int rem = nums[0] % x;
+        for (int num : nums) {
+            if (num % x != rem) return -1;
+        }
 
 int main(){
   
