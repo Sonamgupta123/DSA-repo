ea15be5e852bf55ce27000114d2373c2fefa410c
Sonam Gupta <sonamflower34@gmail.com>
2026-04-29 16:42:41 +0000

    add

--- NewLeet.cpp
+++ NewLeet.cpp
@@ -3,24 +3,7 @@
 #include<vector>
 #include<algorithm>
 using namespace std;
-  int minOperations(vector<vector<int>>& grid, int x) {
-        vector<int> nums;
-        
-        // Step 1: Flatten grid
-        for (auto &row : grid) {
-            for (int val : row) {
-                nums.push_back(val);
-            }
-        }
-        
-        // Step 2: Check feasibility
-        int rem = nums[0] % x;
-        for (int num : nums) {
-            if (num % x != rem) return -1;
-        }
-          // Step 3: Sort
-        sort(nums.begin(), nums.end());
-        
+  
         // Step 4: Choose median
         int median = nums[nums.size() / 2];
         
