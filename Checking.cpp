b0268c30c49eee0a69d7d81ff6ff8ddbc651bae7
Sonam Gupta <sonamflower34@gmail.com>
2026-04-30 17:34:42 +0000

    add5

--- Checking.cpp
+++ Checking.cpp
@@ -28,3 +28,27 @@
                             );
                         }
                     }
+                      // Move Right
+                    if (j + 1 < n) {
+                        int val = grid[i][j+1];
+                        int cost = (val == 0 ? 0 : 1);
+                        int score = val;
+                        
+                        if (c + cost <= k) {
+                            dp[i][j+1][c + cost] = max(
+                                dp[i][j+1][c + cost],
+                                dp[i][j][c] + score
+                            );
+                        }
+                    }
+                }
+            }
+        }
+        
+        int ans = -1;
+        for (int c = 0; c <= k; c++) {
+            ans = max(ans, dp[m-1][n-1][c]);
+        }
+        
+        return ans;
+    }
