8216353058be25f8de42b54fa28c73be54435bce
Sonam Gupta <sonamflower34@gmail.com>
2026-04-30 17:34:11 +0000

    add4

--- Checking.cpp
+++ Checking.cpp
@@ -15,3 +15,16 @@
             for (int j = 0; j < n; j++) {
                 for (int c = 0; c <= k; c++) {
                     if (dp[i][j][c] == -1) continue;
+                     // Move Down
+                    if (i + 1 < m) {
+                        int val = grid[i+1][j];
+                        int cost = (val == 0 ? 0 : 1);
+                        int score = val;
+                        
+                        if (c + cost <= k) {
+                            dp[i+1][j][c + cost] = max(
+                                dp[i+1][j][c + cost],
+                                dp[i][j][c] + score
+                            );
+                        }
+                    }
