02a8f40f2c26b942965f9d46fb4760ee42efb584
Sonam Gupta <sonamflower34@gmail.com>
2026-04-30 17:32:43 +0000

    add2

---
+++ Checking.cpp
@@ -0,0 +1,12 @@
+ int maxPathScore(vector<vector<int>>& grid, int k) {
+        int m = grid.size(), n = grid[0].size();
+        
+        // dp[i][j][c] = max score
+        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(k+1, -1)));
+        
+        dp[0][0][0] = 0; // starting point
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int c = 0; c <= k; c++) {
+                    if (dp[i][j][c] == -1) continue;
