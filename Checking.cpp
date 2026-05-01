622f080f5dbf257ef28b241fa74cf45174cf5d44
Sonam Gupta <sonamflower34@gmail.com>
2026-05-01 14:59:36 +0000

    add3

--- Checking.cpp
+++ Checking.cpp
@@ -5,9 +5,6 @@
  using namespace std;
  
     int main(){
-        vector<vector<int>> grid = {{0,1},{2,0}};
-        int k = 1 ;
-        int ans = maxPathScore(grid , k);
-        cout<<ans<<endl;
+        
         return 0;
     }
