323c51e98251bf01f66618327ee0acf9fd751200
Sonam Gupta <sonamflower34@gmail.com>
2026-05-02 17:41:42 +0000

    add

--- Checking.cpp
+++ Checking.cpp
@@ -3,27 +3,7 @@
  #include <queue>
  #include<algorithm>
  using namespace std;
-  int maxRotateFunction(vector<int>& nums) {
-        int n = nums.size();
-        
-        long sum = 0;
-        long F = 0;
-        
-        // Step 1: sum and F(0)
-        for (int i = 0; i < n; i++) {
-            sum += nums[i];
-            F += (long)i * nums[i];
-        }
-         long maxVal = F;
-        
-        // Step 2: compute F(k)
-        for (int k = 1; k < n; k++) {
-            F = F + sum - (long)n * nums[n - k];
-            maxVal = max(maxVal, F);
-        }
-        
-        return maxVal;
-    }
+ 
     int main(){
         vector<int> nums = {4,3,2,6};
         int ans =  maxRotateFunction(nums);
