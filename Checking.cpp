70d97b26f155c22a02f44de77be8e17ef9ffb964
Sonam Gupta <sonamflower34@gmail.com>
2026-05-02 17:42:00 +0000

    add1

--- Checking.cpp
+++ Checking.cpp
@@ -5,8 +5,5 @@
  using namespace std;
  
     int main(){
-        vector<int> nums = {4,3,2,6};
-        int ans =  maxRotateFunction(nums);
-        cout<<ans<<endl;
-        return 0;
+       
     }
