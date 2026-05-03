80a5595944405c8ee04ec96e34e82e6760afffe8
Sonam Gupta <sonamflower34@gmail.com>
2026-05-03 16:23:23 +0000

    onr

--- Checking.cpp
+++ Checking.cpp
@@ -5,19 +5,7 @@
  using namespace std;
  
 
-                if (digit == 2 || digit == 5 || digit == 6 || digit == 9) {
-                    changed = true;
-                }
-
-                num /= 10;
-            }
-             if (valid && changed) {
-                count++;
-            }
-        }
-
-        return count;
-    }
+              
     int main(){
        int n = 2;
        int ans = rotatedDigits(n);
