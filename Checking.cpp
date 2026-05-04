92b235ea54dba98fbb31a9b118ea40e1a7b9d308
Sonam Gupta <sonamflower34@gmail.com>
2026-05-04 15:52:09 +0000

    Checking.cpp add3

--- Checking.cpp
+++ Checking.cpp
@@ -19,6 +19,8 @@
         }
     }    
     int main(){
-       
+       vector<vector<int>> matrix = {{1,2,3},{4,5,6},{7,8,9}};
+       void ans = rotate(matrix);
+       cout<<ans<<endl;
       return 0;
     }
