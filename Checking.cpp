0ef7ac4985f1566ae2aa915f7aae2a791bd60cb2
Sonam Gupta <sonamflower34@gmail.com>
2026-05-04 15:48:29 +0000

    Checking.cpp

--- Checking.cpp
+++ Checking.cpp
@@ -3,10 +3,7 @@
  #include <queue>
  #include<algorithm>
  using namespace std;
-  bool rotateString(string s, string goal) {
-        if (s.length() != goal.length()) return false;
-        return (s + s).find(goal) != string::npos;
-    }
+  
 
               
     int main(){
