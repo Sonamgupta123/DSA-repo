85326643030205d797ee57a2fe6521aa4325ca47
Sonam Gupta <sonamflower34@gmail.com>
2026-05-04 15:49:26 +0000

    Checking.cpp add1

--- Checking.cpp
+++ Checking.cpp
@@ -3,7 +3,15 @@
  #include <queue>
  #include<algorithm>
  using namespace std;
-  
+   void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+
+        // Step 1: Transpose
+        for(int i = 0; i < n; i++) {
+            for(int j = i; j < n; j++) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
 
               
     int main(){
