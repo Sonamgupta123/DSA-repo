132a74b6baac9b5801ac41242dc3918a8a49c6cd
Sonam Gupta <sonamflower34@gmail.com>
2026-05-04 15:49:49 +0000

    Checking.cpp add2

--- Checking.cpp
+++ Checking.cpp
@@ -13,7 +13,11 @@
             }
         }
 
-              
+           // Step 2: Reverse each row
+        for(int i = 0; i < n; i++) {
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+    }    
     int main(){
        
       return 0;
