fdd6daefc3d33d71ef42bb2b6d3e59bc42951354
Sonam Gupta <sonamflower34@gmail.com>
2026-05-05 17:39:08 +0000

    add

--- Checking.cpp
+++ Checking.cpp
@@ -12,7 +12,25 @@ using namespace std;
             temp = temp->next;
             n++;
         }
+  // Step 2: Make circular
+        temp->next = head;
 
+        // Step 3: Optimize k
+        k = k % n;
+        int stepsToNewHead = n - k;
+
+        // Step 4: Find new tail
+        ListNode* newTail = temp;
+        while (stepsToNewHead--) {
+            newTail = newTail->next;
+        }
+
+        // Step 5: Break circle
+        ListNode* newHead = newTail->next;
+        newTail->next = NULL;
+
+        return newHead;
+    }
 
 int main(){
    
