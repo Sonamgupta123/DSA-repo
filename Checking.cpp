3112d20dfddec217192aeeff99c13b10a901c787
Sonam Gupta <sonamflower34@gmail.com>
2026-05-05 17:38:43 +0000

    addd

--- Checking.cpp
+++ Checking.cpp
@@ -2,6 +2,16 @@
 #include<vector>
 #include<algorithm>
 using namespace std;
+    ListNode* rotateRight(ListNode* head, int k) {
+        if (!head || !head->next || k == 0) return head;
+
+        // Step 1: Find length
+        ListNode* temp = head;
+        int n = 1;
+        while (temp->next) {
+            temp = temp->next;
+            n++;
+        }
 
 
 int main(){
