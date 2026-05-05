aea4d01c74d59e56e197a209ac892b50b2be2527
Sonam Gupta <sonamflower34@gmail.com>
2026-05-05 17:37:20 +0000

    ad

--- Checking.cpp
+++ Checking.cpp
@@ -5,16 +5,6 @@ using namespace std;
 
 
 int main(){
-    vector<vector<int>> matrix = {{1,2,3},{4,5,6},{7,8,9}};
-    
-    rotate(matrix);  // just call it
-
-    // Print rotated matrix
-    for(auto row : matrix){
-        for(auto val : row){
-            cout << val << " ";
-        }
-        cout << endl;
-    }
+   
     return 0;
 }
