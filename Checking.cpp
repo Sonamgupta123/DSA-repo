80d76a313a59131e6847c3ef0205afcbfa0b6a7c
Sonam Gupta <sonamflower34@gmail.com>
2026-05-05 17:35:22 +0000

    add'

--- Checking.cpp
+++ Checking.cpp
@@ -3,21 +3,6 @@
 #include<algorithm>
 using namespace std;
 
-void rotate(vector<vector<int>>& matrix) {
-    int n = matrix.size();
-
-    // Step 1: Transpose
-    for(int i = 0; i < n; i++) {
-        for(int j = i; j < n; j++) {
-            swap(matrix[i][j], matrix[j][i]);
-        }
-    }
-
-    // Step 2: Reverse each row
-    for(int i = 0; i < n; i++) {
-        reverse(matrix[i].begin(), matrix[i].end());
-    }
-}
 
 int main(){
     vector<vector<int>> matrix = {{1,2,3},{4,5,6},{7,8,9}};
