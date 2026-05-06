1c914a32e70b15447b6c58828d04e2fbfde11c9b
Sonam Gupta <sonamflower34@gmail.com>
2026-05-06 17:38:36 +0000

    add1

--- Checking.cpp
+++ Checking.cpp
@@ -2,46 +2,6 @@
 using namespace std;
 
 
-// Function to rotate list
-ListNode* rotateRight(ListNode* head, int k) {
-    if (!head || !head->next || k == 0) return head;
-
-    // Step 1: Find length
-    ListNode* temp = head;
-    int n = 1;
-    while (temp->next) {
-        temp = temp->next;
-        n++;
-    }
-
-    // Step 2: Make circular
-    temp->next = head;
-
-    // Step 3: Optimize k
-    k = k % n;
-    int stepsToNewHead = n - k;
-
-    // Step 4: Find new tail
-    ListNode* newTail = temp;
-    while (stepsToNewHead--) {
-        newTail = newTail->next;
-    }
-
-    // Step 5: Break circle
-    ListNode* newHead = newTail->next;
-    newTail->next = NULL;
-
-    return newHead;
-}
-
-// Function to print list
-void printList(ListNode* head) {
-    while (head) {
-        cout << head->val << " ";
-        head = head->next;
-    }
-    cout << endl;
-}
 
 int main() {
     // Create linked list: 1 -> 2 -> 3 -> 4 -> 5
