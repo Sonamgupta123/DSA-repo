0bbff1db4b39240dacb738afbbc5179f17d211c6
Sonam Gupta <sonamflower34@gmail.com>
2026-05-06 17:38:53 +0000

    add2

--- Checking.cpp
+++ Checking.cpp
@@ -4,22 +4,6 @@ using namespace std;
 
 
 int main() {
-    // Create linked list: 1 -> 2 -> 3 -> 4 -> 5
-    ListNode* head = new ListNode(1);
-    head->next = new ListNode(2);
-    head->next->next = new ListNode(3);
-    head->next->next->next = new ListNode(4);
-    head->next->next->next->next = new ListNode(5);
-
-    int k = 2;
-
-    cout << "Original List: ";
-    printList(head);
-
-    head = rotateRight(head, k);
-
-    cout << "Rotated List: ";
-    printList(head);
-
+   
     return 0;
 }
