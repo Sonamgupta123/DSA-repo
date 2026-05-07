e4f7473c881903b63b0e8945f6ee4353a5ca191e
Sonam Gupta <sonamflower34@gmail.com>
2026-05-07 16:30:31 +0000

    hello1

--- Checking.cpp
+++ Checking.cpp
@@ -5,27 +5,7 @@ using namespace std;
 
 
 int main() {
-    int m, n;
-    cin >> m >> n;
-
-    vector<vector<char>> boxGrid(m, vector<char>(n));
-
-    // Input
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            cin >> boxGrid[i][j];
-        }
-    }
-
-    vector<vector<char>> result = rotateTheBox(boxGrid);
-
-    // Output
-    for (auto &row : result) {
-        for (auto &cell : row) {
-            cout << cell << " ";
-        }
-        cout << endl;
-    }
+    
 
     return 0;
 }
