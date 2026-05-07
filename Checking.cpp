6bc6bf2b806b0f4121a69e2351284cb97b3b6ce8
Sonam Gupta <sonamflower34@gmail.com>
2026-05-07 16:30:05 +0000

    hello

--- Checking.cpp
+++ Checking.cpp
@@ -2,36 +2,7 @@
 #include <vector>
 using namespace std;
 
-vector<vector<char>> rotateTheBox(vector<vector<char>>& boxGrid) {
-    int m = boxGrid.size();
-    int n = boxGrid[0].size();
 
-    // Step 1: Apply gravity (right side)
-    for (int i = 0; i < m; i++) {
-        int empty = n - 1;
-        
-        for (int j = n - 1; j >= 0; j--) {
-            if (boxGrid[i][j] == '*') {
-                empty = j - 1;
-            } 
-            else if (boxGrid[i][j] == '#') {
-                swap(boxGrid[i][j], boxGrid[i][empty]);
-                empty--;
-            }
-        }
-    }
-
-    // Step 2: Rotate matrix
-    vector<vector<char>> result(n, vector<char>(m));
-    
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            result[j][m - 1 - i] = boxGrid[i][j];
-        }
-    }
-
-    return result;
-}
 
 int main() {
     int m, n;
