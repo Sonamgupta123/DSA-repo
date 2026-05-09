14cac16d49122b3a90126d124ff09affeba6f97e
Sonam Gupta <sonamflower34@gmail.com>
2026-05-09 17:38:44 +0000

    hi

--- Checking.cpp
+++ Checking.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+ vector<vector<int>> rotateGrid(vector<vector<int>>& grid, int k) {
+        int m = grid.size();
+        int n = grid[0].size();
 
+        int layers = min(m, n) / 2;
+
+        for (int layer = 0; layer < layers; layer++) {
+            vector<int> elems;
+
+            int top = layer;
+            int left = layer;
+            int bottom = m - layer - 1;
+            int right = n - layer - 1;
 
 
 int main() {
