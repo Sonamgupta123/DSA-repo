50e2bef2a73bb78b5409b858fd5d7f5c67deb8e9
Sonam Gupta <sonamflower34@gmail.com>
2026-05-11 16:24:36 +0000

    add3

--- Checking.cpp
+++ Checking.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
+ vector<int> separateDigits(vector<int>& nums) {
+        vector<int> ans;
+        
+        for (int num : nums) {
+            string s = to_string(num);
+            
+            for (char ch : s) {
+                ans.push_back(ch - '0');
+            }
+        }
+        
+        return ans;
+    }
 
         
 int main() {
