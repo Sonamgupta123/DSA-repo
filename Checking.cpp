3989019655be569d4d9951f7d60c83a113a73c3f
Sonam Gupta <sonamflower34@gmail.com>
2026-05-11 16:27:18 +0000

    final

--- Checking.cpp
+++ Checking.cpp
@@ -1,27 +1,29 @@
 #include <iostream>
 #include <vector>
 using namespace std;
- vector<int> separateDigits(vector<int>& nums) {
-        vector<int> ans;
-        
-        for (int num : nums) {
-            string s = to_string(num);
-            
-            for (char ch : s) {
-                ans.push_back(ch - '0');
-            }
+
+vector<int> separateDigits(vector<int>& nums) {
+    vector<int> ans;
+
+    for (int num : nums) {
+        string s = to_string(num);
+
+        for (char ch : s) {
+            ans.push_back(ch - '0');
         }
-        
-        return ans;
     }
 
-        
+    return ans;
+}
+
 int main() {
-   vector<int> nums = {13,25,83,77};
-   vector<int> ans  = separateDigits(nums);
-   for( x : ans){
-    cout<<x<<endl;
-   }
+    vector<int> nums = {13, 25, 83, 77};
+
+    vector<int> ans = separateDigits(nums);
+
+    for (int x : ans) {
+        cout << x << " ";
+    }
 
     return 0;
 }
