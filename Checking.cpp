e3973da090b9eb390941f35fe3f3767a24a1f8a4
Sonam Gupta <sonamflower34@gmail.com>
2026-05-11 16:23:54 +0000

    add1

--- Checking.cpp
+++ Checking.cpp
@@ -3,26 +3,7 @@
 using namespace std;
 
 
-        // fill top row
-        for (int j = left; j <= right; j++)
-            grid[top][j] = rotated[idx++];
-
-        // fill right column
-        for (int i = top + 1; i <= bottom - 1; i++)
-            grid[i][right] = rotated[idx++];
-
-        // fill bottom row
-        for (int j = right; j >= left; j--)
-            grid[bottom][j] = rotated[idx++];
-
-        // fill left column
-        for (int i = bottom - 1; i >= top + 1; i--)
-            grid[i][left] = rotated[idx++];
-    }
-
-    return grid;
-}
-
+        
 int main() {
     int m, n, k;
 
