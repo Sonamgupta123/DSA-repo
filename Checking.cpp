c9dbee72cfa8424aa714f496779b279575c4d3ec
Sonam Gupta <sonamflower34@gmail.com>
2026-05-11 16:24:08 +0000

    add2

--- Checking.cpp
+++ Checking.cpp
@@ -5,28 +5,7 @@ using namespace std;
 
         
 int main() {
-    int m, n, k;
-
-    cin >> m >> n;
-
-    vector<vector<int>> grid(m, vector<int>(n));
-
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            cin >> grid[i][j];
-        }
-    }
-
-    cin >> k;
-
-    vector<vector<int>> ans = rotateGrid(grid, k);
-
-    for (auto &row : ans) {
-        for (auto &val : row) {
-            cout << val << " ";
-        }
-        cout << endl;
-    }
+   
 
     return 0;
 }
