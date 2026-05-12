1c4afce3869df1aa5fcbc3fe86543cdc0d5525b6
Sonam Gupta <sonamflower34@gmail.com>
2026-05-12 16:50:32 +0000

    check1

--- Checking.cpp
+++ Checking.cpp
@@ -5,13 +5,6 @@ using namespace std;
 
 
 int main() {
-    vector<int> nums = {13, 25, 83, 77};
-
-    vector<int> ans = separateDigits(nums);
-
-    for (int x : ans) {
-        cout << x << " ";
-    }
-
+   
     return 0;
 }
