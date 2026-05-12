a75d3a103491b5d34ea8d8bafe4feedaf7dee785
Sonam Gupta <sonamflower34@gmail.com>
2026-05-12 16:50:08 +0000

    check

--- Checking.cpp
+++ Checking.cpp
@@ -2,19 +2,7 @@
 #include <vector>
 using namespace std;
 
-vector<int> separateDigits(vector<int>& nums) {
-    vector<int> ans;
 
-    for (int num : nums) {
-        string s = to_string(num);
-
-        for (char ch : s) {
-            ans.push_back(ch - '0');
-        }
-    }
-
-    return ans;
-}
 
 int main() {
     vector<int> nums = {13, 25, 83, 77};
