f46e8450f7d0ae56affa5f2a53b9b5b96cdf4d7f
Sonam Gupta <sonamflower34@gmail.com>
2026-05-18 17:15:22 +0000

    add

--- Rotated.cpp
+++ Rotated.cpp
@@ -3,28 +3,7 @@
 
 using namespace std;
 
-int findMin(vector<int>& nums) {
-    int low = 0, high = nums.size() - 1;
 
-    while (low < high) {
-        int mid = low + (high - low) / 2;
-
-        // Minimum is in left part including mid
-        if (nums[mid] < nums[high]) {
-            high = mid;
-        }
-        // Minimum is in right part
-        else if (nums[mid] > nums[high]) {
-            low = mid + 1;
-        }
-        // Duplicates present
-        else {
-            high--;
-        }
-    }
-
-    return nums[low];
-}
 
 int main() {
 
