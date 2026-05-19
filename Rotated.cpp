825daf8f0713d9aefa243c922d37a87eb3fd7434
Sonam Gupta <sonamflower34@gmail.com>
2026-05-19 17:23:34 +0000

    final

--- Rotated.cpp
+++ Rotated.cpp
@@ -1,29 +1,49 @@
 #include <iostream>
 #include <vector>
-#include <unordered_map>
-#include <queue>
 
 using namespace std;
 
- int getCommon(vector<int>& nums1, vector<int>& nums2) {
-        int i = 0, j = 0;
+int getCommon(vector<int>& nums1, vector<int>& nums2) {
 
-        while (i < nums1.size() && j < nums2.size()) {
-            if (nums1[i] == nums2[j]) {
-                return nums1[i];
-            }
-            else if (nums1[i] < nums2[j]) {
-                i++;
-            }
-            else {
-                j++;
-            }
+    int i = 0, j = 0;
+
+    while (i < nums1.size() && j < nums2.size()) {
+
+        if (nums1[i] == nums2[j]) {
+            return nums1[i];
         }
-
-        return -1;
+        else if (nums1[i] < nums2[j]) {
+            i++;
+        }
+        else {
+            j++;
+        }
     }
+
+    return -1;
+}
 
 int main() {
 
-    
+    int n, m;
+
+    cin >> n;
+
+    vector<int> nums1(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> nums1[i];
+    }
+
+    cin >> m;
+
+    vector<int> nums2(m);
+
+    for (int i = 0; i < m; i++) {
+        cin >> nums2[i];
+    }
+
+    cout << getCommon(nums1, nums2);
+
+    return 0;
 }
