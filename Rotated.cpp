839554c038ee7b00e2afcdc996b6fc9c626363c6
Sonam Gupta <sonamflower34@gmail.com>
2026-05-20 15:58:55 +0000

    add1

--- Rotated.cpp
+++ Rotated.cpp
@@ -6,25 +6,7 @@ using namespace std;
 
 int main() {
 
-    int n, m;
-
-    cin >> n;
-
-    vector<int> nums1(n);
-
-    for (int i = 0; i < n; i++) {
-        cin >> nums1[i];
-    }
-
-    cin >> m;
-
-    vector<int> nums2(m);
-
-    for (int i = 0; i < m; i++) {
-        cin >> nums2[i];
-    }
-
-    cout << getCommon(nums1, nums2);
+    
 
     return 0;
 }
