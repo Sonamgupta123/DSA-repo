2f07d3a0fa0d12834410310da749e3316b098563
Sonam Gupta <sonamflower34@gmail.com>
2026-05-23 16:48:11 +0000

    add

--- Rotated.cpp
+++ Rotated.cpp
@@ -4,45 +4,10 @@
 
 using namespace std;
 
-vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
-    int n = A.size();
 
-    vector<int> ans;
-    unordered_set<int> seenA, seenB;
-
-    int common = 0;
-
-    for (int i = 0; i < n; i++) {
-        int a = A[i];
-        int b = B[i];
-
-        seenA.insert(a);
-        seenB.insert(b);
-
-        // If current element of A already appeared in B
-        if (seenB.count(a))
-            common++;
-
-        // If current element of B already appeared in A
-        // avoid double counting when a == b
-        if (a != b && seenA.count(b))
-            common++;
-
-        ans.push_back(common);
-    }
-
-    return ans;
-}
 
 int main() {
-    vector<int> A = {1, 3, 2, 4};
-    vector<int> B = {3, 1, 2, 4};
-
-    vector<int> result = findThePrefixCommonArray(A, B);
-
-    for (int x : result) {
-        cout << x << " ";
-    }
+    
 
     return 0;
 }
