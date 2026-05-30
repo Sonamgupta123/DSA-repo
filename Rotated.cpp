f671b52d405acd25e83ec2be98755ffe6746d7d8
Sonam Gupta <sonamflower34@gmail.com>
2026-05-30 17:18:38 +0000

    add

--- Rotated.cpp
+++ Rotated.cpp
@@ -3,18 +3,7 @@
 
 using namespace std;
 
-bool check(vector<int>& nums) {
-    int count = 0;
-    int n = nums.size();
 
-    for (int i = 0; i < n; i++) {
-        if (nums[i] > nums[(i + 1) % n]) {
-            count++;
-        }
-    }
-
-    return count <= 1;
-}
 
 int main() {
     int n;
